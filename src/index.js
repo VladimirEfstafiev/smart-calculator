2313397b82efb6cf9d9dad8776f46a4fb495e27c
Alex Brov <shlak10@outlook.com>
2018-02-24 22:53:27 +0300

    result

--- src/index.js
+++ src/index.js
@@ -1,27 +1,92 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this._store = initialValue;
+        this._tasks = [];
+        this._operations = {
+            add: {
+                priority: 2,
+                doOperation: (a, b) => {
+                    return a + b;
+                },
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+            },
+            subtract: {
+                priority: 2,
+                doOperation: (a, b) => {
+                    return a - b;
+                },
 
-  multiply(number) {
-    // your implementation
-  }
+            },
+            multiply: {
+                priority: 1,
+                doOperation: (a, b) => {
+                    return a * b;
+                },
 
-  devide(number) {
-    // your implementation
-  }
+            },
+            devide: {
+                priority: 1,
+                doOperation: (a, b) => {
+                    return a / b;
+                },
 
-  pow(number) {
-    // your implementation
-  }
+            },
+            pow: {
+                priority: 0,
+                doOperation: (a, b) => {
+                    return Math.pow(a, b);
+                },
+
+            },
+        }
+    }
+
+    valueOf() {
+        for (let i = 0; i < 3; i++) {
+            for (let k = 0; k < this._tasks.length;) {
+                let currOp = this._operations[this._tasks[k].task];
+                if (currOp.priority == i) {
+                    if (this._tasks[k - 1]) {
+                        this._tasks[k - 1].number = currOp.doOperation(this._tasks[k - 1].number, this._tasks[k].number);
+                    } else {
+                        this._store = currOp.doOperation(this._store, this._tasks[k].number);
+                    }
+                    this._tasks.splice(k, 1);
+
+                } else {
+                    k++
+                }
+            }
+        }
+        return this._store;
+    }
+
+    add(number) {
+        this._tasks.push({ task: "add", number: number });
+        return this;
+    }
+
+    subtract(number) {
+        this._tasks.push({ task: "subtract", number: number });
+        return this;
+    }
+
+    multiply(number) {
+        this._tasks.push({ task: "multiply", number: number });
+        return this;
+    }
+
+    devide(number) {
+        this._tasks.push({ task: "devide", number: number });
+        return this;
+    }
+
+    pow(number) {
+        this._tasks.push({ task: "pow", number: number });
+        return this;
+    }
+
+
 }
 
 module.exports = SmartCalculator;
